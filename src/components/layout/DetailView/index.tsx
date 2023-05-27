71f8de98549e4db1245ca0b85273d16dc26a43ec
Momchil Jambazov <momchil.jambazov@era.io>
2023-05-28 01:18:12 +0300

    fix: remove nested turnaries and prepare for refactor

--- src/components/layout/DetailView/index.tsx
+++ src/components/layout/DetailView/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { TreeNode } from "@/types";
 import { TreeContext } from "@/contexts/TreeContext";
 import { findNodeByKey } from "@/utils/findTreeNode";
@@ -18,7 +18,13 @@ interface TreeViewProps {
   refetch: () => void;
 }
 
-const DetailView: React.FC<TreeViewProps> = ({ tree, currentPath, refetch, keyCount, maxKeys }) => {
+const DetailView: React.FC<TreeViewProps> = ({
+  tree,
+  currentPath,
+  refetch,
+  keyCount,
+  maxKeys,
+}) => {
   const currentNode = findNodeByKey(tree, currentPath);
   const isFolder = currentNode?.type === "folder";
   const isKeyLimitReached = keyCount >= maxKeys;
@@ -27,69 +33,86 @@ const DetailView: React.FC<TreeViewProps> = ({ tree, currentPath, refetch, keyCo
   const { readFile } = useReadS3File();
   const { deleteObject, error, errorCleanup } = useDeleteObject();
 
+  const [fileData, setFileData] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (!isFolder && currentNode) {
+      readFile(currentNode?.key)
+        .then((response) => response?.Body?.transformToString())
+        .then((data) => setFileData(data))
+        .catch((error) => {
+          console.error("Error reading S3 file:", error);
+          setFileData(undefined);
+        });
+    } else {
+      setFileData(undefined);
+    }
+  }, [currentNode, isFolder, readFile]);
+
   const handleDelete = (key: string, event?: React.MouseEvent) => {
     event?.stopPropagation();
     deleteObject(key);
   };
 
-  const [fileData, setData] = useState<string | undefined>();
-
-  if (!fileData && !isFolder && currentNode) {
-    readFile(currentNode?.key).then(r => r?.Body?.transformToString()).then(d => setData(d))
-  }
-  if (fileData && isFolder) {
-    setData(undefined);
-  }
+  const isEmpty = !currentDirObjects.length
 
   return (
     <div className="detail-view">
-      {error && <Toast message={`Error deleting S3 object: ${error?.message}`} type="danger" callback={errorCleanup} />}
+      {error && (
+        <Toast
+          message={`Error deleting S3 object: ${error?.message}`}
+          type="danger"
+          callback={errorCleanup}
+        />
+      )}
       <Breadcrumbs
         currentPath={currentPath}
         refetch={refetch}
         showCreateOptions={isFolder && !isKeyLimitReached}
       />
-      {!isFolder ?
+      {!isFolder && (
         <div className="card">
           {fileData}
-        </div> :
-        !!currentPath && !currentDirObjects.length && isFolder ? (
-          <div className="card">
-            <p style={{ textAlign: "center", width: "100%" }}>Folder is empty</p>
-          </div>
-        ) : (
-          <div className="grid card">
-            {currentDirObjects.map((childNode) => {
-              const icon = childNode?.type === "folder" ? "📁" : "📄";
-              return (
-                <div key={childNode.key} className="position-relative grid-item">
-                  <div
-                    className="thumbnail"
-                    onClick={() => setCurrentPath(childNode.key)}
-                  >
-                    <span className="icon">{icon}</span>
-                    {childNode.name}
-                  </div>
-                  <Popover
-                    className="delete-popover"
-                    content={
-                      <div>
-                        You are about to delete this {childNode.type}, are you sure?
-                        <Button
-                          className="danger mx"
-                          onClick={(e) => handleDelete(childNode.key, e)}
-                        >
-                          Yes
-                        </Button>
-                      </div>
-                    }
-                    trigger={<Button className="delete-button">X</Button>}
-                  />
+        </div>
+      )}
+      {isEmpty && isFolder && (
+        <div className="card">
+          <p style={{ textAlign: "center", width: "100%" }}>Folder is empty</p>
+        </div>
+      )}
+      {!isEmpty && isFolder && (
+        <div className="grid card">
+          {currentDirObjects.map((childNode) => {
+            const icon = childNode?.type === "folder" ? "📁" : "📄";
+            return (
+              <div key={childNode.key} className="position-relative grid-item">
+                <div
+                  className="thumbnail"
+                  onClick={() => setCurrentPath(childNode.key)}
+                >
+                  <span className="icon">{icon}</span>
+                  {childNode.name}
                 </div>
-              );
-            })}
-          </div>
-        )}
+                <Popover
+                  className="delete-popover"
+                  content={
+                    <div>
+                      You are about to delete this {childNode.type}, are you sure?
+                      <Button
+                        className="danger mx"
+                        onClick={(e) => handleDelete(childNode.key, e)}
+                      >
+                        Yes
+                      </Button>
+                    </div>
+                  }
+                  trigger={<Button className="delete-button">X</Button>}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
